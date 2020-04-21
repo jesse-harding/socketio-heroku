0e6782e95614158bbb6dc64cafe9e8b56346e85f
Jesse Harding <jesse.alexander.harding@gmail.com>
2020-04-21 13:59:53 -0400

    Add files via upload

---
+++ telepcomp.ino
@@ -0,0 +1,72 @@
+//define pin connected to pushbutton
+//const int buttonPin = 2;
+////define pin connected to potentiometer
+//const int potPin = A0;
+////declare pin connected to LED (we are using the built-in LED)
+//const int ledPin = 13;
+
+String inString;
+
+
+#include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+//declare and initialize button state (outgoing on serial connection)
+//int buttonState = 0;
+//
+////declare and initialize potentiometer state (outgoing on serial connection)
+//int potState = 0;
+
+//declare and initialize LED state (incoming from serial connection)
+float state = 0;
+
+void setup() {
+// initialize the LED pin as an output:
+//pinMode(ledPin, OUTPUT);
+//// initialize the pushbutton pin as an input:
+//pinMode(potPin, INPUT);
+//pinMode(buttonPin, INPUT);
+Serial.begin(9600);
+myservo.attach(9);
+}
+
+void loop() {
+// read the state of the pushbutton value:
+//potState = analogRead(potPin);
+//// check if the pushbutton is pressed.
+//buttonState = digitalRead(buttonPin); //momentary button
+
+//output the two variables to the serial connection with an "&" symbol between (it could be any character of your choosing, as long as the same character is used to split the two variables apart on the other side of the serial connection).
+//this ampersand symbol is a delimiter which separates our two variables for decoding.
+//Serial.print(potState);
+//Serial.print("&");
+//Serial.println(buttonState);
+
+  //check to see if there is a connection 
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    //if so, read the incoming data
+//    state = Serial.read(); // Reads the data from the serial port
+    inString = Serial.readStringUntil('\n');
+  inString.trim();
+  state = inString.toInt();
+ }
+
+if (state != 0){
+myservo.write(state);
+}
+else{
+  myservo.write(90);
+}
+
+// if (state == '0') {
+//  digitalWrite(ledPin, LOW); // Turn LED OFF
+//  myservo.write(170);
+//  state = 0;
+// }
+// else if (state == '1') {
+//  digitalWrite(ledPin, HIGH); //Turn LED ON
+//  myservo.write(10);
+//  state = 0;
+// } 
+
+
+}
